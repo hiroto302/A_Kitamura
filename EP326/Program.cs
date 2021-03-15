36c5c31e58d078c20b5c69403cf1a7cd7698f6b3
hiroto302 <i.am.a.hiroto@gmail.com>
2021-03-15 15:37:20 +0900

    Linq 例題

---
+++ EP326/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EP326
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = { -1, -10, -5, -40, -15};
+            // n配列の全ての要素数を１０増やし、その結果０以上となる要素数の個数を求める
+            int i = n.Select(n => n +10).Where(n => n >= 0).Count();
+            Console.WriteLine(i + "個");
+        }
+    }
+}
